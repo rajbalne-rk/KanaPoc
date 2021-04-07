096a197fe1c71a4da99180d0034527f2cd38bb46
rajbalne-rk <66076248+rajbalne-rk@users.noreply.github.com>
2021-04-07 19:45:48 -0400

    Add files via upload

--- js/kana/members.js
+++ js/kana/members.js
@@ -1,1 +1,27 @@
+﻿$(document).ready(function () {
+    var url=""
+    var components = new Vue({
+        el: '#members',
+        data: {
+            membersList: {}
+        },
+        created: function () {
+            var self = this;
+            axios.get("https://frg6v92z6f.execute-api.us-east-1.amazonaws.com/test/members", {
+                headers: { Pragma: 'no-cache' }
+            })
+                .then(function (response) {
+                    components.membersList = response.data.membersList;
+                })
+                .catch(
+                    function (response) {
+                        if (isDebugMode())
+                            console.log(response.data);
+                    });
+        },
+        methods: {
+            
+        }
 
+    });
+});
